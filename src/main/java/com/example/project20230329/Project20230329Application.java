7617569af072c03ff0399fe101dcfbf6426cf6ef
shinae <qkqjzz486@naver.com>
2023-03-29 21:25:53 +0900

    init

---
+++ src/main/java/com/example/project20230329/Project20230329Application.java
@@ -0,0 +1,13 @@
+package com.example.project20230329;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project20230329Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project20230329Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project20230329/Project20230329ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project20230329;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project20230329ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
